b2d7a9ce9ac3497764a046be19cc230e6d507f62
jdmedlock <jdmedlock@gmail.com>
2023-02-15 14:24:15 -0600

    Feature: Add styling + update conversion calculations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
 function App() {
+  const [celciusTemp, setCelciusTemp] = useState("")
+  const [fahrenheitTemp, setFahrenheitTemp] = useState("")
+
+  const updateCelciusTemp = (e) => {
+    setFahrenheitTemp("")
+    setCelciusTemp(e.target.value)
+  } 
+
+  const updateFahrenheitTemp = (e) => {
+    setCelciusTemp("")
+    setFahrenheitTemp(e.target.value)
+  } 
+
+  const resetTemperatures = () => {
+    setCelciusTemp("")
+    setFahrenheitTemp("")
+  }
+
+  const convertToCelcius = () => {
+    return (fahrenheitTemp - 32) * (5/9)
+  }
+
+  const convertToFahrenheit = () => {
+    return ((celciusTemp * (9/5)) + 32)
+  }
+
+  const convertTemperature = () => {
+    if (celciusTemp !== '') {
+      setFahrenheitTemp(convertToFahrenheit())
+      return
+    }
+
+    if (fahrenheitTemp !== '') {
+      setCelciusTemp(convertToCelcius())
+      return
+    }
+
+    throw new Error('Invalid celcius and fahrenheit temperatures')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="App-header">
+      <h1>
+        Temperature Converter
+      </h1>
+      
+      <label htmlFor="celcius" className="temp-label">Enter Celcius:</label>
+      <input type="text" name="fahrenheit" placeholder="Celcius temp."
+        value={ celciusTemp }
+        onChange={ (e) => updateCelciusTemp(e) }
+      />
+      
+      <label htmlFor="fahrenheit" className="temp-label">Enter Fahrenheit:</label>
+      <input type="text" name="fahrenheit" placeholder="Fahrenheit temp."
+        value={ fahrenheitTemp }
+        onChange={ (e) => updateFahrenheitTemp(e) }
+      />
+
+      <span>
+        <button type="button" className="btn"
+          onClick={() => resetTemperatures() }>
+          Reset
+        </button>
+        <button type="button" className=""
+          onClick={() => convertTemperature() }>
+          Convert
+        </button>
+      </span>
+
+    </section>
+  )
 }
 
-export default App;
+export default App
