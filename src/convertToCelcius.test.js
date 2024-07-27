d21c274b701d5a4e4b81b07996797c62ecfe80eb
jdmedlock <jdmedlock@gmail.com>
2024-07-27 08:56:25 -0500

    Feature: Add new test

--- src/convertToCelcius.test.js
+++ src/convertToCelcius.test.js
@@ -1,5 +1,9 @@
-import { convertToCelcius } from './util/convertToCelcius'
+import { convertToCelcius, convertToFahrenheit } from './util/temperatureConversions.js'
 
 test('fahrenheit to celcius conversion works properly', () => {
   expect(convertToCelcius(32)).toBe(0)
+})
+
+test('celcius to fahrenheit conversion', () => {
+  expect(convertToFahrenheit(0)).toBe(32)
 })
